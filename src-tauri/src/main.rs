9fdcc234133c6f8c2160de8afc84217e3d8971e0
anotherhollow1125 <anotherhollow1125@gmail.com>
2022-07-18 22:55:01 +0900

    自由に選べるようになりました

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,68 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+#[macro_use]
+extern crate serde;
+
+use std::fs;
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum Entry {
+    #[serde(rename = "file")]
+    File { name: String, path: String },
+    #[serde(rename = "dir")]
+    Dir {
+        name: String,
+        path: String,
+        // entries: Vec<Entry>,
+    },
+}
+
+#[tauri::command]
+fn get_entries(path: &str) -> Result<Vec<Entry>, String> {
+    let entries = fs::read_dir(path).map_err(|e| format!("{}", e))?;
+
+    let res = entries
+        .filter_map(|entry| -> Option<Entry> {
+            let entry = entry.ok()?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            let path = entry.path().to_string_lossy().to_string();
+            let type_ = entry.file_type().ok()?;
+
+            if type_.is_dir() {
+                Some(Entry::Dir { name, path })
+            } else if type_.is_file() {
+                Some(Entry::File { name, path })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(res)
+}
+
+#[tauri::command]
+fn get_home_dir() -> Result<String, String> {
+    let res = dirs::home_dir()
+        .ok_or("Home Dir couldn't be found.".to_string())?
+        .to_string_lossy()
+        .to_string();
+
+    Ok(res)
+}
+
+#[tauri::command]
+fn hello(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![hello, get_entries, get_home_dir])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,81 @@
 import { useState, useEffect } from 'react';
 import ReactPlayer from "react-player";
-import { desktopDir, join } from '@tauri-apps/api/path';
+import { join } from '@tauri-apps/api/path';
 import { convertFileSrc } from '@tauri-apps/api/tauri';
+import { invoke } from '@tauri-apps/api/tauri';
+
+type Entry = {
+  type: 'dir' | 'file';
+  name: string;
+  path: string;
+};
+
+type Entries = Array<Entry>;
 
 const App = () => {
-  const [src, setSrc] = useState<string>('');
-  const [player, setPlayer] = useState<JSX.Element>();
+  const [src, setSrc] = useState<string | null>(null);
+  const [dir, setDir] = useState<string | null>(null);
+  const [player, setPlayer] = useState<JSX.Element | null>(null);
+  const [entries, setEntries] = useState<Entries | null>(null);
 
   useEffect(() => {
-    const fn = async () => {
-      const desktopDirPath = await desktopDir();
-      const url = convertFileSrc(await join(desktopDirPath, src));
-      const player = <ReactPlayer url={url} controls={true} />;
-      setPlayer(player);
-    };
-    fn();
+    (async () => {
+      const homeDir = await invoke<string>("get_home_dir")
+        .catch(err => {
+          console.error(err);
+          return null;
+        });
+
+      if (!homeDir) {
+        return;
+      }
+
+      setDir(homeDir);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (!src) {
+      return;
+    }
+
+    const url = convertFileSrc(src);
+    const player = <ReactPlayer url={url} controls={true} />;
+    setPlayer(player);
   }, [src]);
+
+  useEffect(() => {
+    (async () => {
+      const entries = await invoke<Entries>("get_entries", { path: dir })
+        .catch(err => {
+          console.error(err);
+          return null;
+        });
+
+      setEntries(entries);
+    })();
+  }, [dir]);
+
+  const entry_list = entries ? <ul>
+    {entries.map(entry => {
+      if (entry.type == "dir") {
+        return <li key={entry.path} onClick={() => setDir(entry.path)}>{entry.name}</li>;
+      } else {
+        return <li key={entry.path} onClick={() => setSrc(entry.path)}>{entry.name}</li>;
+      }
+    })}
+  </ul> : null;
 
   return (
     <>
       <h1>React Player</h1>
       {player}
       <br />
-      {src}
+      src: {src ?? '(not selected)'}
       <br />
+      dir: {dir ?? ''}
       <br />
-      <input type="text" value={src} onChange={e => setSrc(e.target.value)} />
+      {entry_list}
     </>
   );
 }
